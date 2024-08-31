f8ea8a2c9dfa4cba377177a8c282b308eec09ee2
moniq-e <otavioschimith124@gmail.com>
2024-08-30 23:10:28 -0300

    alinhar iguais

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const rollButt = document.querySelector("button#buttRoll")
 const increase = document.querySelector("button#increase")
 const decrease = document.querySelector("button#decrease")
+
 const amount = document.querySelector("input#amount")
 const balance = document.querySelector("span#balance")
 
-const slot1 = document.querySelector("span#s1")
-const slot2 = document.querySelector("span#s2")
-const slot3 = document.querySelector("span#s3")
+const slot1 = document.querySelector("input#s1")
+const slot2 = document.querySelector("input#s2")
+const slot3 = document.querySelector("input#s3")
 
-const img1 = document.querySelector("img#s1")
-const img2 = document.querySelector("img#s2")
-const img3 = document.querySelector("img#s3")
+const img1 = document.querySelector("img#i1")
+const img2 = document.querySelector("img#i2")
+const img3 = document.querySelector("img#i3")
 
 const slots = [slot1, slot2, slot3]
 
-let interval
+let interval, generated = [0, 0, 0]
 
 decrease.addEventListener("click", e => {
-    if(amount.value - 1 < 0) return
-    amount.value = parseFloat(amount.value) - 1
+    amount.value = Math.max(0, parseFloat(amount.value) - 1)
 })
+
 increase.addEventListener("click", e => {
-    amount.value = parseFloat(amount.value) + 1
+    amount.value = Math.min(parseFloat(balance.innerText), parseFloat(amount.value) + 1)
 })
 
 rollButt.addEventListener("click", e => {
-    if (!amount.value || amount.value < 0) return
+    if (!amount.value || parseFloat(amount.value) < 0) return
 
-    if (amount.value <= parseFloat(balance.innerText)) {
+    if (parseFloat(amount.value) <= parseFloat(balance.innerText)) {
+        generated = generated.map(() => Math.floor((Math.random() * 5) + 1))
+        
+        //align the two equals numbers
+        generated.sort(Math.random() < 0.5 ? (a, b) => a - b : (a, b) => b - a)
+        if (generated[1] == generated[2]) {
+            let temp = generated[0]
+            generated[0] = generated[2]
+            generated[2] = temp
+        }
+
         balance.innerText -= amount.value
 
         slot1.setAttribute("rolls", 0)
@@ -40,10 +51,11 @@ rollButt.addEventListener("click", e => {
 
         clearInterval(interval)
         interval = setInterval(roll, 20)
-        rollButt.disabled = true
+
         rollButt.style.filter = "saturate(0%)"
-        increase.disabled = true;
-        decrease.disabled = true;
+        rollButt.disabled = true
+        increase.disabled = true
+        decrease.disabled = true
         amount.disabled = true
     }
 })
@@ -54,6 +66,8 @@ function roll() {
         img1.src = "images/imagem" + slot1.innerText + ".png"
         slot1.setAttribute("rolls", parseInt(slot1.getAttribute("rolls")) + 1)
     } else {
+        slot1.innerText = generated[0]
+        img1.src = "images/imagem" + slot1.innerText + ".png"
         slot1.setAttribute("done", true)
     }
 
@@ -62,6 +76,8 @@ function roll() {
         img2.src = "images/imagem" + slot2.innerText + ".png"
         slot2.setAttribute("rolls", parseInt(slot2.getAttribute("rolls")) + 1)
     } else {
+        slot2.innerText = generated[1]
+        img2.src = "images/imagem" + slot2.innerText + ".png"
         slot2.setAttribute("done", true)
     }
 
@@ -70,15 +86,17 @@ function roll() {
         img3.src = "images/imagem" + slot3.innerText + ".png"
         slot3.setAttribute("rolls", parseInt(slot3.getAttribute("rolls")) + 1)
     } else {
+        slot3.innerText = generated[2]
+        img3.src = "images/imagem" + slot3.innerText + ".png"
         slot3.setAttribute("done", true)
     }
 
     if (slot1.getAttribute("done") && slot2.getAttribute("done") && slot3.getAttribute("done")) {
+        rollButt.style.filter = ""
         rollButt.disabled = false
-        rollButt.style.filter = "saturate(50%)"
         amount.disabled = false
-        increase.disabled = false;
-        decrease.disabled = false;
+        increase.disabled = false
+        decrease.disabled = false
         check()
         clearInterval(interval)
     }
