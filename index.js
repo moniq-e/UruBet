4849b571626d64b72676c090203ebafc143d9187
Pedro Samuel <108874841+PSGentil@users.noreply.github.com>
2024-09-02 11:31:13 -0300

    soltando a carniça

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const increase = document.querySelector("button#increase")
 const decrease = document.querySelector("button#decrease")
 const infoButt = document.querySelector("button#buttHelp")
 const closeButt = document.querySelector("button#close")
+const autoRollButt = document.querySelector("button#buttAutoRoll")
 
 const help = document.querySelector("div#help")
+const winAmount = document.querySelector("#winAmount")
+const restart = document.querySelector("#restart")
 
 const amount = document.querySelector("input#amount")
 const balance = document.querySelector("span#balance")
 
-const slot1 = document.querySelector("input#s1")
-const slot2 = document.querySelector("input#s2")
-const slot3 = document.querySelector("input#s3")
+const input1 = document.querySelector("input#s1")
+const input2 = document.querySelector("input#s2")
+const input3 = document.querySelector("input#s3")
 
 const windows = document.querySelectorAll("span.slot")
 
@@ -19,31 +22,45 @@ const img1 = document.querySelector("img#i1")
 const img2 = document.querySelector("img#i2")
 const img3 = document.querySelector("img#i3")
 
-const slots = [slot1, slot2, slot3]
+const inputs = [input1, input2, input3]
 
-let interval, generated = [0, 0, 0]
+let interval, autoRoll = false, generated = [0, 0, 0]
 
-decrease.addEventListener("click", e => {
-    amount.value = Math.max(0, parseFloat(amount.value) - 1)
+balance.innerText = localStorage.getItem("urubet") ?? 10
+
+restart.addEventListener("click", _ => {
+    localStorage.removeItem("urubet")
+    location.reload()
 })
 
-increase.addEventListener("click", e => {
+decrease.addEventListener("click", _ => {
+    amount.value = Math.max(0, parseFloat(amount.value) - 1)
+})
+increase.addEventListener("click", _ => {
     amount.value = Math.min(parseFloat(balance.innerText), parseFloat(amount.value) + 1)
 })
 
-infoButt.addEventListener("click", e => {
+infoButt.addEventListener("click", _ => {
     help.style.display = "flex"
 })
-closeButt.addEventListener("click", e => {
+closeButt.addEventListener("click", _ => {
     help.style.display = "none"
 })
+autoRollButt.addEventListener("click", _ => {
+    if (autoRoll) {
+        autoRollButt.querySelector("img").classList.remove("autoRoll")
+    } else {
+        autoRollButt.querySelector("img").classList.add("autoRoll")
+    }
+    autoRoll = !autoRoll
+})
 
-rollButt.addEventListener("click", e => {
+rollButt.addEventListener("click", _ => {
+    winAmount.classList.remove("winnimation")
     for (const w of windows) {
         w.classList.remove("win")
     }
-    
-    
+
     if (!amount.value || parseFloat(amount.value) < 0) return
 
     if (parseFloat(amount.value) <= parseFloat(balance.innerText)) {
@@ -62,13 +79,13 @@ rollButt.addEventListener("click", e => {
 
         balance.innerText -= amount.value
 
-        slot1.setAttribute("rolls", 0)
-        slot2.setAttribute("rolls", 0)
-        slot3.setAttribute("rolls", 0)
+        input1.setAttribute("rolls", 0)
+        input2.setAttribute("rolls", 0)
+        input3.setAttribute("rolls", 0)
 
-        slot1.removeAttribute("done")
-        slot2.removeAttribute("done")
-        slot3.removeAttribute("done")
+        input1.removeAttribute("done")
+        input2.removeAttribute("done")
+        input3.removeAttribute("done")
 
         clearInterval(interval)
         interval = setInterval(roll, 70)
@@ -82,40 +99,40 @@ rollButt.addEventListener("click", e => {
 })
 
 function roll() {
-    if (slot1.getAttribute("rolls") < 30) {
-        slot1.innerText = Math.floor((Math.random() * 5) + 1)
-        img1.src = "images/imagem" + slot1.innerText + ".png"
-        slot1.setAttribute("rolls", parseInt(slot1.getAttribute("rolls")) + 1)
+    if (input1.getAttribute("rolls") < 30) {
+        input1.innerText = Math.floor((Math.random() * 5) + 1)
+        img1.src = "images/imagem" + input1.innerText + ".png"
+        input1.setAttribute("rolls", parseInt(input1.getAttribute("rolls")) + 1)
     } else {
-        slot1.innerText = generated[0]
-        img1.src = "images/imagem" + slot1.innerText + ".png"
+        input1.innerText = generated[0]
+        img1.src = "images/imagem" + input1.innerText + ".png"
         img1.style.animation = "spinFinish .2s ease-in-out"
-        slot1.setAttribute("done", true)
+        input1.setAttribute("done", true)
     }
 
-    if (slot2.getAttribute("rolls") < 40) {
-        slot2.innerText = Math.floor((Math.random() * 5) + 1)
-        img2.src = "images/imagem" + slot2.innerText + ".png"
-        slot2.setAttribute("rolls", parseInt(slot2.getAttribute("rolls")) + 1)
+    if (input2.getAttribute("rolls") < 40) {
+        input2.innerText = Math.floor((Math.random() * 5) + 1)
+        img2.src = "images/imagem" + input2.innerText + ".png"
+        input2.setAttribute("rolls", parseInt(input2.getAttribute("rolls")) + 1)
     } else {
-        slot2.innerText = generated[1]
-        img2.src = "images/imagem" + slot2.innerText + ".png"
+        input2.innerText = generated[1]
+        img2.src = "images/imagem" + input2.innerText + ".png"
         img2.style.animation = "spinFinish .2s ease-in-out"
-        slot2.setAttribute("done", true)
+        input2.setAttribute("done", true)
     }
 
-    if (slot3.getAttribute("rolls") < 50) {
-        slot3.innerText = Math.floor((Math.random() * 5) + 1)
-        img3.src = "images/imagem" + slot3.innerText + ".png"
-        slot3.setAttribute("rolls", parseInt(slot3.getAttribute("rolls")) + 1)
+    if (input3.getAttribute("rolls") < 50) {
+        input3.innerText = Math.floor((Math.random() * 5) + 1)
+        img3.src = "images/imagem" + input3.innerText + ".png"
+        input3.setAttribute("rolls", parseInt(input3.getAttribute("rolls")) + 1)
     } else {
-        slot3.innerText = generated[2]
-        img3.src = "images/imagem" + slot3.innerText + ".png"
+        input3.innerText = generated[2]
+        img3.src = "images/imagem" + input3.innerText + ".png"
         img3.style.animation = "spinFinish .2s ease-in-out"
-        slot3.setAttribute("done", true)
+        input3.setAttribute("done", true)
     }
 
-    if (slot1.getAttribute("done") && slot2.getAttribute("done") && slot3.getAttribute("done")) {
+    if (input1.getAttribute("done") && input2.getAttribute("done") && input3.getAttribute("done")) {
         rollButt.style.filter = ""
         rollButt.disabled = false
         amount.disabled = false
@@ -127,20 +144,37 @@ function roll() {
 }
 
 function check() {
-    if (slot1.innerText == slot2.innerText && slot2.innerText == slot3.innerText) {
+    winAmount.innerText = 0
+    let won = false
+    if (input1.innerText == input2.innerText && input2.innerText == input3.innerText) {
+        won = true
         win()
     }
-    if (slots.some(s => s.innerText == 1)) {
-        slots.forEach(s => {
+    if (inputs.some(s => s.innerText == 1)) {
+        let bonus = 0
+        inputs.forEach(s => {
             if (s.innerText == 1) {
-                balance.innerText = parseFloat(balance.innerText) + amount.value * 1.5
+                bonus += amount.value * 1.5
             }
         })
+        balance.innerText = parseFloat(balance.innerText) + bonus
+        winAmount.innerText = parseFloat(winAmount.innerText) + bonus
     }
+    if (autoRoll) {
+        setTimeout(() => rollButt.click(), won ? 1500 : 500)
+    }
+    if (winAmount.innerText != 0) {
+        winAmount.classList.add("winnimation")
+        winAmount.innerText = "+" + winAmount.innerText
+    }
+    localStorage.setItem("urubet", balance.innerText)
 }
 
 function win() {
-    balance.innerText = parseFloat(balance.innerText) + amount.value * parseInt(slot1.innerText) * 2
+    let bonus = amount.value * parseInt(input1.innerText) * 2
+    balance.innerText = parseFloat(balance.innerText) + bonus
+    winAmount.innerText = parseFloat(winAmount.innerText) + bonus
+
     for (const w of windows) {
         w.classList.add("win")
     }
